187514daad98507e5bf0a77149f3072f68bb2c4b
Sporard <pierresabard@gmail.com>
2022-09-13 22:48:38 +0200

    templating

--- src/app.php
+++ src/app.php
@@ -1,11 +1,35 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
 
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'world']));
+
+function is_leap_year(int $year = null)
+{
+    if (is_null($year)) {
+        $year = date('Y');
+    }
+
+    return $year % 400 === 0 || ($year % 4 === 0 && !($year % 100 === 0));
+}
+
+$routes->add(
+    'leap_year',
+    new Route('/is_leap_year/{year}', [
+        'name' => 'world',
+        '_controller' => function (Request $request) {
+            if (is_leap_year($request->attributes->get('year'))){
+                return new Response('Yes it is');
+            }
+
+            return new Response('No it\'s not', 400);
+        }
+    ])
+);
 $routes->add('bye', new Route('/bye'));
 
 
