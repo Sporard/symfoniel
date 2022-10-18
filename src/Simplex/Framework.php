7d82c84bbd1b35ac5f31d9e4120a548696d53325
Sporard <pierresabard@gmail.com>
2022-10-18 21:27:19 +0200

    symplify framework

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,26 +2,45 @@
 
 namespace Simplex;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\ErrorListener;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 class Framework extends HttpKernel
 {
     /**
-     * @param EventDispatcher $eventDispatcher
-     * @param ControllerResolverInterface $controllerResolver
-     * @param $requestStack
-     * @param ArgumentResolverInterface $argumentResolver
+     * @param $routes
      */
-    public function __construct(private EventDispatcher             $eventDispatcher,
-                                private ControllerResolverInterface $controllerResolver,
-                                protected                           $requestStack,
-                                private ArgumentResolverInterface   $argumentResolver,
-    )
+    public function __construct($routes)
     {
-        parent::__construct($this->eventDispatcher, $this->controllerResolver, $this->requestStack, $this->argumentResolver);
+        $controllerResolver = new ControllerResolver();
+        $argumentsResolver = new ArgumentResolver();
+
+
+        $requestStack = new RequestStack();
+
+        $context = new RequestContext();
+        $matcher = new UrlMatcher($routes, $context);
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber(new ContentLengthListener());
+        $dispatcher->addSubscriber(new GoogleListener());
+        $dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+        $dispatcher->addSubscriber(new ErrorListener(
+            'Calendar\Controller\ErrorController::exception'
+        ));
+        $dispatcher->addSubscriber(new ResponseListener('UTF-8'));
+        $dispatcher->addSubscriber(new StringResponseListener());
+
+        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentsResolver);
+
     }
 
 }
--- web/FrontController.php
+++ web/FrontController.php
@@ -3,44 +3,14 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Simplex\Framework;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 use Symfony\Component\HttpKernel\HttpCache\Store;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
-
-$controllerResolver = new ControllerResolver();
-$argumentsResolver = new ArgumentResolver();
 
 
 $routes = include __DIR__ . '/../src/app.php';
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
-
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new Simplex\GoogleListener());
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new ErrorListener(
-    'Calendar\Controller\ErrorController::exception'
-));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
 
 
-$framework = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentsResolver);
-$framework = new HttpCache(
-    $framework,
-    new Store(__DIR__ . '/../cache')
-);
-$response = $framework->handle($request);
-$response->send();
+$framework = new Framework($routes);
+$framework->handle($request)->send();
