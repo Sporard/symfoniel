71c3961b91a92b049c625e6caa4ae338a3f46de2
Sporard <pierresabard@gmail.com>
2022-10-19 00:36:06 +0200

    dependency injection

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,45 +2,9 @@
 
 namespace Simplex;
 
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\EventListener\ErrorListener;
-use Symfony\Component\HttpKernel\EventListener\ResponseListener;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
 
 class Framework extends HttpKernel
 {
-    /**
-     * @param $routes
-     */
-    public function __construct($routes)
-    {
-        $controllerResolver = new ControllerResolver();
-        $argumentsResolver = new ArgumentResolver();
-
-
-        $requestStack = new RequestStack();
-
-        $context = new RequestContext();
-        $matcher = new UrlMatcher($routes, $context);
-
-        $dispatcher = new EventDispatcher();
-        $dispatcher->addSubscriber(new ContentLengthListener());
-        $dispatcher->addSubscriber(new GoogleListener());
-        $dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-        $dispatcher->addSubscriber(new ErrorListener(
-            'Calendar\Controller\ErrorController::exception'
-        ));
-        $dispatcher->addSubscriber(new ResponseListener('UTF-8'));
-        $dispatcher->addSubscriber(new StringResponseListener());
-
-        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentsResolver);
-
-    }
 
 }
--- web/FrontController.php
+++ web/FrontController.php
@@ -2,15 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Simplex\Framework;
+use Simplex\StringResponseListener;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\HttpCache\HttpCache;
-use Symfony\Component\HttpKernel\HttpCache\Store;
 
-
-$routes = include __DIR__ . '/../src/app.php';
+$container = include __DIR__ . '/../src/container.php';
 $request = Request::createFromGlobals();
-
-
-$framework = new Framework($routes);
-$framework->handle($request)->send();
+$container->register('listener.string_response', StringResponseListener::class);
+$container->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', [new Reference('listener.string_response')]);
+$container->setParameter('routes', include __DIR__ . '/../src/app.php');
+$container->get('framework')->handle($request)->send();
