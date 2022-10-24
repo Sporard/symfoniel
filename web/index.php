540402542755be4c0258180c0ee4baa785fcbf49
Sporard <pierresabard@gmail.com>
2022-10-24 23:14:15 +0200

    cleaning

--- web/index.php
+++ web/index.php
@@ -6,11 +6,11 @@ use Simplex\StringResponseListener;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpFoundation\Request;
 
-$container = include __DIR__ . '/../src/container.php';
+$container = include __DIR__ . '/../config/services.php';
 $request = Request::createFromGlobals();
 $container->register('listener.string_response', StringResponseListener::class);
 $container->setParameter('templatesDir', __DIR__ . '/../src/templates');
 $container->getDefinition('dispatcher')
     ->addMethodCall('addSubscriber', [new Reference('listener.string_response')]);
-$container->getDefinition('matcher')->replaceArgument(0, include __DIR__ . '/../src/routes.php');
+$container->getDefinition('matcher')->replaceArgument(0, include __DIR__ . '/../config/routes.php');
 $container->get('framework')->handle($request)->send();
