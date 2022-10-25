778d27e088a3d7f87767fe83fc4803921055fa01
Sporard <pierresabard@gmail.com>
2022-10-25 21:30:19 +0200

    add auto json for non string response

--- src/Simplex/Listener/ResponseListener.php
+++ src/Simplex/Listener/ResponseListener.php
@@ -14,7 +14,15 @@ class ResponseListener implements EventSubscriberInterface
 
         if (is_string($response)) {
             $event->setResponse(new Response($response));
+        } else {
+            $data = [
+                "data" => $response
+            ];
+            $jsonResponse = new Response(json_encode($data));
+            $jsonResponse->headers->set('Content-type', 'application/json');
+            $event->setResponse($endResponse);
         }
+
     }
 
     public static function getSubscribedEvents()
